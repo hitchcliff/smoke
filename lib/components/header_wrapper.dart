13df3228d01a2c446fea6c5d32ace0c30b83b074
hitchcliff <hitchcliff1@gmail.com>
2024-12-27 08:06:39 +0800

    header wrapper

---
+++ lib/components/header_wrapper.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:single_store_ecommerce/components/circular_container.dart';
+import 'package:single_store_ecommerce/components/curve_edges.dart';
+import 'package:single_store_ecommerce/utils/constants/colors.dart';
+
+class HeaderWrapper extends StatelessWidget {
+  const HeaderWrapper({super.key, required this.child, this.height = 400});
+
+  final Widget child;
+  final double? height;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: CurveEdges(),
+      child: Container(
+        color: MyColors.primary,
+        child: SizedBox(
+          height: height,
+          child: Stack(children: [
+            const Positioned(
+              top: -150,
+              right: -250,
+              child: CircularContainer(
+                backgroundColor: MyColors.white,
+                opacity: 0.1,
+              ),
+            ),
+            const Positioned(
+              top: 100,
+              right: -300,
+              child: CircularContainer(
+                backgroundColor: MyColors.white,
+                opacity: 0.1,
+              ),
+            ),
+            child,
+          ]),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:single_store_ecommerce/components/circular_container.dart';
-import 'package:single_store_ecommerce/components/curve_edge_widget.dart';
-import 'package:single_store_ecommerce/utils/constants/colors.dart';
+import 'package:single_store_ecommerce/components/header_wrapper.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -14,32 +12,7 @@ class Home extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              CurveEdgeWidget(
-                child: Container(
-                  color: MyColors.primary,
-                  child: const SizedBox(
-                    height: 400,
-                    child: Stack(children: [
-                      Positioned(
-                        top: -150,
-                        right: -250,
-                        child: CircularContainer(
-                          backgroundColor: MyColors.white,
-                          opacity: 0.1,
-                        ),
-                      ),
-                      Positioned(
-                        top: 100,
-                        right: -300,
-                        child: CircularContainer(
-                          backgroundColor: MyColors.white,
-                          opacity: 0.1,
-                        ),
-                      ),
-                    ]),
-                  ),
-                ),
-              ),
+              HeaderWrapper(height: 300, child: Container()),
             ],
           ),
         ),
