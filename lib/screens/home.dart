7d2d822be731b6212b7854da5a654171306b9c70
hitchcliff <hitchcliff1@gmail.com>
2024-12-27 22:52:11 +0800

    cleanup imports

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,11 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:single_store_ecommerce/components/cart_counter_icon.dart';
 import 'package:single_store_ecommerce/components/home_appbar.dart';
-import 'package:single_store_ecommerce/components/my_app_bar.dart';
-import 'package:single_store_ecommerce/components/header_wrapper.dart';
-import 'package:single_store_ecommerce/utils/constants/colors.dart';
-import 'package:single_store_ecommerce/utils/constants/text_strings.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -18,6 +12,7 @@ class Home extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
+              // app bar
               HomeAppbar(),
             ],
           ),
